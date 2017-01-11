935ff414fd592f8830d8ef2cb8c19435fa4952c4
Julian <julian@contingent.dk>
2017-01-11 18:05:02 +0100

    downgrade to es5

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,11 @@
-const gen = require(`../src/generate`)
-const program = require('commander')
+var gen = require('../src/generate')
+var program = require('commander')
 
 program
   .version('1.0.0')
-  .option('-i, --swagger-file [value]', `path or URL to swagger file`)
-  .option('-c, --class-name [value]', `Name the one generated class. Defaults to GeneratedAPI`)
-  .option('-o, --output [value]', `output file path. Defaults to the class name`)
+  .option('-i, --swagger-file [value]', 'path or URL to swagger file')
+  .option('-c, --class-name [value]', 'Name the one generated class. Defaults to GeneratedAPI')
+  .option('-o, --output [value]', 'output file path. Defaults to the class name')
   .parse(process.argv);
 
 gen(program.swaggerFile, program.className, program.output)
