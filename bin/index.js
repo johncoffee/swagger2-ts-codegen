ca0709b66e994ef8bc19e106fd6d377ef637ac06
Julian <julian@contingent.dk>
2017-01-11 18:08:50 +0100

    version number from package.json

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,9 @@
 var gen = require('../src/generate')
 var program = require('commander')
+var packageJson = require('../package.json')
 
 program
-  .version('1.0.0')
+  .version(packageJson.version)
   .option('-i, --swagger-file [value]', 'path or URL to swagger file')
   .option('-c, --class-name [value]', 'Name the one generated class. Defaults to GeneratedAPI')
   .option('-o, --output [value]', 'output file path. Defaults to the class name')
